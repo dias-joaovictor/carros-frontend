bbfda1e1f3637efc7ca88772009659ab4396c225
Joao Victor Dias <kvj1610@hotmail.com>
2024-12-09 21:05:35 +0100

    Implement crud

---
+++ src/services/CarroService.ts
@@ -0,0 +1,11 @@
+import { CrudService } from './CrudService';
+import { Carro } from '../models/Carro';
+
+const baseURL = process.env.REACT_APP_BACKEND_URL;
+
+export const carroService = new CrudService<Carro, number>(
+  'api/carros',
+  {
+    baseURL
+  }
+);
