38661bdc679a2183da63d7ec471747205dda5a88
Joao Victor Dias <kvj1610@hotmail.com>
2024-12-09 22:48:07 +0100

    Implement crud

--- src/layouts/SidebarLayout/Header/index.tsx
+++ src/layouts/SidebarLayout/Header/index.tsx
@@ -67,9 +67,7 @@ function Header() {
         divider={<Divider orientation="vertical" flexItem />}
         alignItems="center"
         spacing={2}
-      >
-        <HeaderMenu />
-      </Stack>
+      />
       <Box display="flex" alignItems="center">
         <HeaderButtons />
         <HeaderUserbox />
